83c553156f063e4cf266b6692bd581089ab561ae
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-11-25 19:45:26 +0545

    app.js routh path change

--- src/Routing/App.js
+++ src/Routing/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <>
         <Navbars />
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route exact path="/" component={Home} />
         </Switch>
         <Footer />
       </>
